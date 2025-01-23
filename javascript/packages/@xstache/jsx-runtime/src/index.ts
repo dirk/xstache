a453852a4fccf9d3471e749de69b4881160de755
Dirk Gadsden <dirk@esherido.com>
2025-01-22 19:49:46 -0800

    Start building JSX runtime

--- javascript/packages/@xstache/jsx-runtime/src/index.ts
+++ javascript/packages/@xstache/jsx-runtime/src/index.ts
@@ -1,3 +1,26 @@
+interface JsxRuntime {
+    jsx(type: any, props: unknown, key?: any): any;
+    jsxs(type: any, props: unknown, key?: any): any;
+    Fragment: any;
+}
+
+class Context {
+    constructor(private readonly data: any) {}
+
+    public v(...keys: string[]) {
+        let value = this.data;
+        for (const key of keys) {
+            const next = value[key];
+            if (next) {
+                value = next;
+            } else {
+                return undefined;
+            }
+        }
+        return value;
+    }
+}
+
 export interface CodeObject {
     xs: Function;
 }
@@ -5,7 +28,7 @@ export interface CodeObject {
 export class Template {
     constructor(private readonly compiled: CodeObject) {}
 
-    public render() {
-        this.compiled.xs();
+    public render(jsxRuntime: JsxRuntime, data: any) {
+        return this.compiled.xs(jsxRuntime, new Context(data));
     }
 }
--- javascript/packages/@xstache/jsx/src/index.test.tsx
+++ javascript/packages/@xstache/jsx/src/index.test.tsx
@@ -1,14 +1,30 @@
+import React from "react";
+import * as jsxRuntime from "react/jsx-runtime";
 import { describe, expect, test } from "vitest";
 import parse from "@xstache/parse";
 
-import { compileToString } from "./index.js";
+import { compileToString, compileToTemplate } from "./index.js";
 
-// describe("compileToTemplate", () => {
-//     test("compiles", () => {
-//         const nodeList = parse("<div foo={bar}>Hello {name}</div>\n<input />");
-//         console.log(compileToTemplate(nodeList));
-//     });
-// });
+describe("compileToTemplate", () => {
+    test.for([
+        ["attributes", "<div foo={bar} />", <div foo="baz" />],
+        [
+            "contents in elements",
+            "<span>foo {bar}</span>",
+            <span>foo {"baz"}</span>,
+        ],
+        [
+            "missing contents for elements",
+            "<span>foo {baz}</span>",
+            <span>foo {undefined}</span>,
+        ],
+    ])("compiles and renders with %s", ([_, input, expected]) => {
+        const nodeList = parse(input);
+        const template = compileToTemplate(nodeList, { pretty: true });
+        const output = template.render(jsxRuntime, { bar: "baz" });
+        expect(output).toEqual(expected);
+    });
+});
 
 describe("compileToString", () => {
     test("compiles", () => {
