215f6f3a1e88d50c7aaf15f9c9c1f28aaa292982
Dirk Gadsden <dirk@esherido.com>
2025-07-26 10:09:15 -0700

    Add CLI for compiling to JavaScript that renders HTML

--- javascript/packages/@xstache/html/src/index.ts
+++ javascript/packages/@xstache/html/src/index.ts
@@ -3,11 +3,28 @@ import { Template } from "@xstache/html-runtime";
 
 import Compiler from "./compile.js";
 
-export function compileToString(nodeList: NodeList): string {
-    return new Compiler(true).compileToString(nodeList);
+export function compileToString(
+    nodeList: NodeList,
+    options: ModuleOptions = {},
+): string {
+    const implementation = new Compiler(true).compileToString(nodeList);
+    return options.module ? wrapInModule(implementation) : implementation;
 }
 
 export function compileToTemplate(nodeList: NodeList) {
     const implementation = new Compiler(true).compileToFunction(nodeList);
     return new Template(implementation);
+}
+
+type ModuleOptions = Partial<{
+    module: boolean;
+}>;
+
+function wrapInModule(implementation: string) {
+    return [
+        `import { Template } from "@xstache/html-runtime";`,
+        `const implementation = ${implementation};`,
+        "export default new Template(implementation);",
+        "",
+    ].join("\n");
 }
