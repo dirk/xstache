c5ebddae8265999ec669eb4762cde5d219391354
Dirk Gadsden <dirk@esherido.com>
2025-07-26 13:04:50 -0700

    Better exclusions for Vitest coverage and Prettier

--- javascript/vitest.config.ts
+++ javascript/vitest.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         projects: ["packages/xstache", "packages/@xstache/*"],
+        coverage: {
+            exclude: [...coverageConfigDefaults.exclude, "**/dist/**"],
+        },
     },
 });
