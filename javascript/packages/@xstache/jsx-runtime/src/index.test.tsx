60c44c57135e5a6d8f43dcb39b63fcaa69b88621
Dirk Gadsden <dirk@esherido.com>
2025-07-27 12:28:04 -0700

    Add unit tests for JavaScript JSX runtime package

---
+++ javascript/packages/@xstache/jsx-runtime/src/index.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import * as jsxRuntime from "react/jsx-runtime";
+import { renderToString } from "react-dom/server";
+import { describe, test, expect } from "vitest";
+
+import { componentFactory } from "./index";
+
+describe("componentFactory", () => {
+    test("creates a component function", () => {
+        const Component = componentFactory(
+            (context) => context.value(["name"]),
+            jsxRuntime,
+        );
+        const element = <Component name="John" />;
+        expect(renderToString(element)).toMatchInlineSnapshot(`"John"`);
+    });
+
+    test("handles nested contexts", () => {
+        const Component = componentFactory(
+            (context, jsxRuntime) =>
+                context.section(
+                    ["items"],
+                    (context, index) => (
+                        <span key={index}>{context.value(["name"])}</span>
+                    ),
+                    jsxRuntime,
+                ),
+            jsxRuntime,
+        );
+        const element = (
+            <Component items={[{ name: "Item 1" }, { name: "Item 2" }]} />
+        );
+        expect(renderToString(element)).toMatchInlineSnapshot(`"<span>Item 1</span><span>Item 2</span>"`);
+    });
+});
